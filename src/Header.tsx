b791700726f2bd269fbfbf6f3d91ffb26125fc93
Rafael Schmidt <up202000166@fe.up.pt>
2024-04-03 20:07:25 -0300

    trabalho no header collapse

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,7 @@
-import { Anchor, Button, Divider, Flex, Group, Stack } from "@mantine/core";
+import { AppShell, AppShellAside, Burger, Button, Divider, Flex, Group, Stack } from "@mantine/core";
 import { ColorSchemeToggle } from "./ColorSchemeToggle";
 import { Link, Outlet } from "@tanstack/react-router";
+import { useDisclosure } from "@mantine/hooks";
 
 const links = [
   { link: "/equipe", label: "Equipe" },
@@ -10,6 +11,7 @@ const links = [
 ];
 
 export function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
   const headerItems = links.map((link, i) => (
     <Link key={i} to={link.link}>
       <Button variant="subtle" color="gray" radius="md">
@@ -21,16 +23,23 @@ export function Header() {
   return (
     <>
       <Stack w="100dvw" gap={0}>
-        <Flex h="8vh" justify="space-between" px="6rem" align="center">
+        <Flex h="8vh" justify="space-between" px="xl" align="center">
+        <Burger
+                opened={opened}
+                onClick={toggle}
+                hiddenFrom="xs"
+                size="sm"
+              />
           <Link to="/">
             <Button variant="transparent" color="gray" fz="xl" lts="0.2rem">
               LAMDEC
             </Button>
           </Link>
-          <nav>
-            <Group>{headerItems}</Group>
-          </nav>
+            <Group visibleFrom="xs" wrap="nowrap" gap={0}>
+              {headerItems}
+            </Group>
           <ColorSchemeToggle />
+
         </Flex>
         <Divider />
       </Stack>
